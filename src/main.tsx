c53f58889109468f2d33231c8d3190e9423447b5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-12 18:54:48 -0300

    init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.tsx";
-import "./assets/css/index.css";
+import "./assets/css/Index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
