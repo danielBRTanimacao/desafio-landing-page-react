fbe47da99b14391608902bc816ede9925639da22
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-14 17:49:15 -0300

    creating a desing to a headphone

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 export default () => {
     return (
         <>
-            <header>Header Principal</header>
+            <header className="header">
+                {/* <h1 className="display-1">PRISTINE SOUND OF 100 YEARS</h1> */}
+                <h1 className="display-1">PRISTINE SOUND</h1>
+                <h1 className="display-1">
+                    OF 100 <span className="mr-start">YEARS</span>
+                </h1>
+            </header>
         </>
     );
 };
