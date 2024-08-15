95e49ac2e695659ef33440f2ef3210e66ada7dc5
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-15 13:30:51 -0300

    creating and updating design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,13 @@ export default () => {
     return (
         <>
             <header className="header">
-                {/* <h1 className="display-1">PRISTINE SOUND OF 100 YEARS</h1> */}
                 <h1 className="display-1">PRISTINE SOUND</h1>
                 <h1 className="display-1">
                     OF 100 <span className="mr-start">YEARS</span>
                 </h1>
+                <div className="div-margin-p">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
             </header>
         </>
     );
