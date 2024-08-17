048d371db513df64f346c57ffbd44e0ae94dc615
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-17 10:42:47 -0300

    init design final

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,20 @@ export default () => {
     return (
         <>
             <header>
-                <h1>Header</h1>
+                <article>
+                    <h1>Tudo fica melhor com pizza</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Assumenda magnam optio cupiditate veniam dolorum totam
+                    </p>
+                    <aside>
+                        <a href="#">Order now</a>
+                        <a href="#">learn more</a>
+                    </aside>
+                </article>
+                <div>
+                    <img src="#" alt="img-pizza" />
+                </div>
             </header>
         </>
     );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,30 @@
 export default () => {
     return (
         <>
-            <h1>Navigation</h1>
+            <nav>
+                <h2>Lorem</h2>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Menu</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                    <li>
+                        <form action="#" method="get">
+                            <a href="#qId">quest</a>
+                            <input type="search" name="q" id="qId" />
+                        </form>
+                    </li>
+                </ul>
+                <a href="#">CRIAR CONTA</a>
+            </nav>
         </>
     );
 };
