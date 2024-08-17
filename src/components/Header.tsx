7d79ebdeca1e8dea1dcac6acce5d892e1489b330
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-17 18:00:23 -0300

    pre concluid principal page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,57 @@
 export default () => {
     return (
         <>
-            <header>
-                <article>
-                    <h1>Tudo fica melhor com pizza</h1>
-                    <p>
+            <header className="header-hero">
+                <article className="small-article">
+                    <h1 className="title-hero">
+                        Tudo fica melhor com{" "}
+                        <span className="title-pizza">PIZZA🍕</span>
+                    </h1>
+                    <p className="legends division-paragraph">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Assumenda magnam optio cupiditate veniam dolorum totam
+                        Assumenda magnam optio cupiditate veniam Lorem, ipsum.
+                        Lorem, ipsum dolor.
                     </p>
-                    <aside>
-                        <a href="#">Order now</a>
-                        <a href="#">learn more</a>
+                    <aside className="aside-btns">
+                        <a href="#" className="btn-rounded">
+                            Fazer um pedido
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="22"
+                                height="22"
+                                fill="currentColor"
+                                className="bi bi-chevron-right"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+                                />
+                            </svg>
+                        </a>
+                        <a href="#" className="btn-rounded none-bg">
+                            Veja mais
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="22"
+                                height="22"
+                                fill="currentColor"
+                                className="bi bi-chevron-right"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+                                />
+                            </svg>
+                        </a>
                     </aside>
                 </article>
                 <div>
-                    <img src="#" alt="img-pizza" />
+                    <img
+                        src="https://via.placeholder.com/800x500"
+                        alt="img-pizza"
+                    />
                 </div>
             </header>
         </>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,10 +2,12 @@ export default () => {
     return (
         <>
             <nav className="nav">
-                <h2>Lorem</h2>
+                <h2 className="orange-title">Lorem</h2>
                 <ul className="nav">
                     <li>
-                        <a href="#">Home</a>
+                        <a href="#" className="active-link" aria-current="page">
+                            Home
+                        </a>
                     </li>
                     <li>
                         <a href="#">Menu</a>
@@ -17,13 +19,32 @@ export default () => {
                         <a href="#">Contact</a>
                     </li>
                     <li>
-                        <form action="#" method="get">
-                            <a href="#qId">quest</a>
-                            <input type="search" name="q" id="qId" />
+                        <form className="flex-form" action="#" method="get">
+                            <a href="#qId" className="orange-title">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="22"
+                                    height="22"
+                                    fill="currentColor"
+                                    className="bi bi-search"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                                </svg>
+                            </a>
+                            <input
+                                className="flex-form input-search"
+                                type="search"
+                                name="q"
+                                id="qId"
+                                placeholder="Search"
+                            />
                         </form>
                     </li>
                 </ul>
-                <a href="#">CRIAR CONTA</a>
+                <a href="#" className="btn-rounded">
+                    CRIAR CONTA
+                </a>
             </nav>
         </>
     );
