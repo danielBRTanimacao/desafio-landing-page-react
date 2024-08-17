5dac5eac6e0d0683e5d6bf8e9783acfee8222a10
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-17 18:22:54 -0300

    making style main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Main from "./components/Main";
 
 export default () => {
     return (
         <>
             <Nav />
             <Header />
+            <Main />
         </>
     );
 };
---
+++ src/components/Main.tsx
@@ -0,0 +1,46 @@
+export default () => {
+    return (
+        <>
+            <main className="main-hero">
+                <div>
+                    <h2 className="small-article">
+                        O amante perfeito é aquele que vira pizza
+                    </h2>
+                    <a href="#">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="22"
+                            height="22"
+                            fill="currentColor"
+                            className="bi bi-chevron-right"
+                            viewBox="0 0 16 16"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+                            />
+                        </svg>
+                    </a>
+                </div>
+                <article className="article-blockots">
+                    <div className="blockots">
+                        <h3>100+</h3>
+                        <p>Pizza Comum</p>
+                    </div>
+                    <div className="blockots">
+                        <h3>100+</h3>
+                        <p>regula pizza</p>
+                    </div>
+                    <div className="blockots">
+                        <h3>100+</h3>
+                        <p>regula pizza</p>
+                    </div>
+                    <div className="blockots">
+                        <h3>100+</h3>
+                        <p>regula pizza</p>
+                    </div>
+                </article>
+            </main>
+        </>
+    );
+};
