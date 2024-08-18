232318320db792b4c6af508f7eb9a2e148160bcb
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-08-18 13:01:41 -0300

    creating links and final remaking design

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -45,6 +45,21 @@ export default () => {
                 <a href="#" className="btn-rounded">
                     CRIAR CONTA
                 </a>
+                <a href="#" className="dropdown">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="36"
+                        height="36"
+                        fill="currentColor"
+                        className="bi bi-list"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+                        />
+                    </svg>
+                </a>
             </nav>
         </>
     );
